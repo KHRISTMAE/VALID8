afc6574a0f5e95de23d74c84c5e1984d8cf5955e
helloitsjea <cadaliniryljean@gmail.com>
2025-03-12 14:26:24 +0800

    Home Controller and Security Configuration

--- src/main/java/com/example/demo/controller/GoogleSheetsController.java
+++ src/main/java/com/example/demo/controller/GoogleSheetsController.java
@@ -1,10 +1,10 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.service.GoogleSheetsService;
-
 
 @RestController
 @RequestMapping("/sheets")
@@ -12,6 +12,7 @@ public class GoogleSheetsController {
 
     private final GoogleSheetsService googleSheetsService;
 
+    @Autowired
     public GoogleSheetsController(GoogleSheetsService googleSheetsService) {
         this.googleSheetsService = googleSheetsService;
     }
